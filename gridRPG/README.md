ab2e8120910b7bf2afe50e036519a4ce92631aec
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-03-28 12:46:24 +0200

    Remove old garbage

--- gridRPG/README.md
+++
@@ -1,2 +0,0 @@
-This project is meant to be an example of folder structure, do not build any projects here
-Copy the root (ahaha funny) folder and rename it if you are lazy :)
--- gridRPG/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include "../include/gameInterface/gameInterface.hpp"
-
-int main()
-{
-    IsoRPG::Game g{};
-    int res{g.loop()};
-    return res;
-}
