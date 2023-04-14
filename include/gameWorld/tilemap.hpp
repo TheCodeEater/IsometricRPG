3ebf632b8d9d2077477fe69451d530efdfcb698f
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-04-14 14:52:08 +0200

    Created classes for handling the tilemap: the class is structured based on the SFML tutorial. The class keeps tracks of the vertices, of the texture, tile size and number of tiles i nthe 2D world axis
    Setting up the tilemap tile size and texture is done by the mapTiles function, which maps them from a matrix containing, for each tile, the id of its texture rect to be taken from a texture file holding all the tiles textures

--- include/gameWorld/tilemap.hpp
+++ include/gameWorld/tilemap.hpp
@@ -20,19 +20,26 @@ namespace IsoRPG {
 /// creates the vertices array. Takes the number of tiles per axis, their size in pixels (must be the same for texture and screen), texture 
 //////////////////////////////
 
+class mapTile{//class to associate tilemap id to texture coordinates
+    static std::map<Tiles::ID,sf::FloatRect> tiles_pos_;//tiles position
+    public:
+        mapTile()=default;
+        sf::FloatRect operator() (Tiles::ID);
+};
+
 class Tilemap : public sf::Drawable,
                 public sf::Transformable {
  private:
   sf::VertexArray vertices_;
   sf::Vector2u N_tiles_;
-  sf::Vector2u tileSize_;
+  float tileSize_;
   sf::Texture& texture_;
 
  public:
-  Tilemap(sf::Vector2u n_tiles,sf::Vector2u tileSize,sf::Texture const& texture);
+  Tilemap(sf::Vector2u n_tiles,float tileSize,sf::Texture const& texture);
 
-  template<size_t x, size_t y>
-  void mapTiles(std::array<std::array<int,y>,x>);
+  template<size_t x_, size_t y_>
+    void Tilemap::mapTiles(std::array<std::array<Tiles::ID,x_>,y_> map);
 
   private:
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
